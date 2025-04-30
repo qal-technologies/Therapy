c28b8ec0dfafe9d8a953e4207544a30e82122891
Qal Tech <130528093+qal-technologies@users.noreply.github.com>
2025-04-30 06:35:53 +0100

    new page

--- js/signup.js
+++ js/signup.js
@@ -24,6 +24,26 @@ document.addEventListener('DOMContentLoaded', () => {
                 forms[formToShow].classList.remove('fadeOut');
             }, 300);
         });
-    })
+    });
+    //     tabs.forEach(tab => {
+    //         tab.addEventListener('click', () => {
+    //             const formToShow = tab.dataset.form;
+    //             if (tab.classList.contains('active')) return;
+
+    //             // Switch active tab
+    //             document.querySelector('.tab.active').classList.remove('active');
+    //             tab.classList.add('active');
+
+    //             // Fade out current form
+    //             const currentActiveForm = document.querySelector('.form-container:not(.fadeOut)');
+    //             currentActiveForm.classList.add('fadeOut');
+
+    //             // Fade in new form
+    //             setTimeout(() => {
+    //                 forms[formToShow].classList.remove('fadeOut');
+    //             }, 300);
+    //         });
+    //     });
+    // });
 }
 );
