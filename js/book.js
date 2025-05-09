4a03db24ad65ad8a0d4e63a40254dfc4c48dd378
Qal Tech <130528093+qal-technologies@users.noreply.github.com>
2025-05-09 14:21:49 +0100

    Update book.js

--- js/book.js
+++ js/book.js
@@ -97,7 +97,7 @@ const DOM = {
   extra: document.querySelector("#session-plan .name .extra"),
   description: document.querySelector('#session-plan .intro'),
   formGroup: document.querySelector(".form-group#session"),
-  ticket: document.querySelector(".form-container .lower h1.ticket"),
+  ticket: document.querySelector(".form-section .lower h1.ticket"),
 registerForm: document.querySelector(".form-section"),
 };
 
