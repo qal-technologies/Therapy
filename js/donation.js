cff78b3ad1ec2762ea75d79d2c08d61ddab131ef
Qal Tech <130528093+qal-technologies@users.noreply.github.com>
2025-08-08 20:27:02 +0100

    ll

--- js/donation.js
+++ js/donation.js
@@ -24,7 +24,7 @@ const stories = [
     name: "Medical",
     stories: [
       {
-        images: ["/src/images/logo.jpg",], location: "Tallahassee, FL", title: "Being Weezy Ain't Easy - Help Jessie Get New Lungs", target: "50K", raised: "233,563", donations: "7.7K",
+        images: ["/src/images/medical/01/IMG-20250807-WA0054.jpg", "/src/images/medical/01/IMG-20250807-WA0055.jpg", "/src/images/medical/01/IMG-20250807-WA0056.jpg", "/src/images/medical/01/IMG-20250807-WA0057.jpg", "/src/images/medical/01/IMG-20250807-WA0058.jpg"], location: "Tallahassee, FL", title: "Being Weezy Ain't Easy - Help Jessie Get New Lungs", target: "50K", raised: "233,563", donations: "7.7K",
         percentage: "99%", description: `My name is Joshua Blood, and I’m reaching out to ask for support for my incredible wife, Jessie. Jessie has been diagnosed with Interstitial Lung Disease, caused by Mixed Connective Tissue Disease, a rare and aggressive condition that continues to destroy her lungs even with treatment. Despite her strength and determination, her lung function has dropped below 45%, and doctors have confirmed that she will need a double lung transplant within the next year to survive.
 
 Jessie is a devoted mother to our 8-year-old daughter, and a loving stepmom to our 12 and 14-year-old children. Without this transplant, she may never get to see them graduate, go to prom, or walk down the aisle. We are fighting with everything we have to give her that chance.
@@ -61,7 +61,7 @@ From the bottom of our hearts, thank you.
         }
       },
       {
-        images: ["/src/images/logo.jpg",], location: "Monoroeville, PA", title: "Support Tracey's Fight Against Severe Illness", target: "25K", raised: "24,086", donations: "314",
+        images: ["/src/images/medical/02/IMG-20250807-WA0066.jpg", "/src/images/medical/02/IMG-20250807-WA0067.jpg", "/src/images/medical/02/IMG-20250807-WA0068.jpg", "/src/images/medical/02/IMG-20250807-WA0072.jpg"], location: "Monoroeville, PA", title: "Support Tracey's Fight Against Severe Illness", target: "25K", raised: "24,086", donations: "314",
         percentage: "96%", description: "Very long description", organizer: "Mike Warywoda", just: {
           number: 91,
           top: [
@@ -83,7 +83,7 @@ From the bottom of our hearts, thank you.
         }
       },
       {
-        images: ["/src/images/logo.jpg",], location: "Goochland, VA", title: "Support Parker Forcke's Fight Against Stage 4 Colon Cancer", target: "40K", raised: "35,483", donations: "332",
+        images: ["/src/images/medical/03/IMG-20250807-WA0070.jpg", "/src/images/medical/03/IMG-20250807-WA0065.jpg", "/src/images/medical/03/IMG-20250807-WA0065.jpg", "/src/images/medical/03/IMG-20250807-WA0071.jpg", "/src/images/medical/03/IMG-20250807-WA0073.jpg" ], location: "Goochland, VA", title: "Support Parker Forcke's Fight Against Stage 4 Colon Cancer", target: "40K", raised: "35,483", donations: "332",
         percentage: "89%", description: "Very long description", organizer: "Stacey Forcke", just: {
           number: 178,
           top: [
@@ -92,6 +92,28 @@ From the bottom of our hearts, thank you.
             { name: "Emily Branch", amount: "100", time: "30 mins" },
 
             { name: "Thomas Layman", amount: "25", time: "1 hr" },
+          ],
+          upper: [
+            { name: "Jennifer Hall", amount: "50", rank: "Recent donation", time: "16 mins" },
+            { name: "Edythe Synder Sanders", amount: "25", rank: "Recent donation", time: "36 mins" },
+            { name: "Christopher Parsons", amount: "10", rank: "Recent donation", time: "40 mins" },
+            { name: "Christopher Youngman", amount: "25", rank: "Recent donation", time: "50 mins" },
+          ],
+          others: [
+
+          ]
+        }
+      },
+      {
+        images: ["/src/images/medical/04/IMG-20250807-WA0076.jpg", "/src/images/medical/04/IMG-20250807-WA0077.jpg", "/src/images/medical/04/IMG-20250807-WA0078.jpg"], location: "New York, NY", title: "Support Sheila Jordan's Hospice Care", target: "120K", raised: "118,147", donations: "1.1K",
+        percentage: "98%", description: `Very long description`, organizer: "Tracy Jordan", just: {
+          number: 0,
+          top: [
+            { name: "Ben Ruben-Schnirman", amount: "5", rank: "First donation", time: "16 mins" },
+
+            { name: "Francis Dance", amount: "10", time: "27 mins" },
+
+            { name: "Emilie Conway", amount: "115", time: "36 mins" },
           ],
           upper: [
             { name: "Jennifer Hall", amount: "50", rank: "Recent donation", time: "16 mins" },
@@ -110,15 +132,15 @@ From the bottom of our hearts, thank you.
     name: "Memorial",
     stories: [
       {
-        images: ["/src/images/logo.jpg",], location: "Tallahassee, FL", title: "Being Weezy Ain't Easy - Help Jessie Get New Lungs", target: "50K", raised: "233,563", donations: "7.7K",
-        percentage: "99%", description: "Very long description", organizer: "Josh Blood", just: {
-          number: 74,
+        images: ["/src/images/logo.jpg",], location: "Honey Hill Mobile Home Park, FL", title: "Honor Grandma Madie's Legacy and Wishes", target: "24K", raised: "20,345", donations: "625",
+        percentage: "85%", description: `Very long description`, organizer: "Shanika Bradshaw", just: {
+          number: 299,
           top: [
-            { name: "Jennifer Hall", amount: "50", rank: "Recent donation", time: "16 mins" },
+            { name: "Brandolyn Jones", amount: "20", rank: "Recent donation", time: "16 mins" },
 
-            { name: "Amber Hodges", amount: "1,200", rank: "Top donation", time: "1 hr" },
+            { name: "Andrea Bland", amount: "25", time: "24 mins" },
 
-            { name: "Anonymous", amount: "100", rank: "First donation", time: "30 mins" },
+            { name: "Sabrina Philips", amount: "100", time: "33 mins" },
           ],
           upper: [
             { name: "Jennifer Hall", amount: "50", rank: "Recent donation", time: "16 mins" },
@@ -132,15 +154,14 @@ From the bottom of our hearts, thank you.
         }
       },
       {
-        images: ["/src/images/logo.jpg",], location: "Monoroeville, PA", title: "Support Tracey's Fight Against Severe Illness", target: "25K", raised: "24,086", donations: "314",
-        percentage: "96%", description: "Very long description", organizer: "Mike Warywoda", just: {
-          number: 91,
+        images: ["/src/images/memory/02/IMG-20250807-WA0085.jpg", "/src/images/memory/02/IMG-20250807-WA0084.jpg", "/src/images/memory/02/IMG-20250807-WA0083.jpg", "/src/images/memory/02/IMG-20250807-WA0086.jpg"], location: "Santa Ana, CA", title: "In Memory of Leticia Placencia Alvarado: Funeral Fund", target: "24K", raised: "13,062", donations: "290",
+        percentage: "54%", description: `Very long description`, organizer: "Wendy Perez", just: {
+          number: 288,
           top: [
-            { name: "Stacey Snead-Peterson", amount: "25", rank: "Recent donation", time: "30 mins" },
-            { name: "Heather Cope", amount: "500", rank: "Top donation", time: "16 mins" },
+            { name: "Jose Fontao", amount: "100", rank: "First donation", time: "30 mins" },
+            { name: "Darlene Fuertes", amount: "10", time: "20 mins" },
 
-            { name: "Madison Hickman", amount: "100", rank: "First donation", time: "1 hr" },
-
+            { name: "Ricardo De Los Keyes", amount: "50", time: "26 mins" },
           ],
           upper: [
             { name: "Jennifer Hall", amount: "50", rank: "Recent donation", time: "16 mins" },
@@ -154,15 +175,15 @@ From the bottom of our hearts, thank you.
         }
       },
       {
-        images: ["/src/images/logo.jpg",], location: "Goochland, VA", title: "Support Parker Forcke's Fight Against Stage 4 Colon Cancer", target: "40K", raised: "35,483", donations: "332",
-        percentage: "89%", description: "Very long description", organizer: "Stacey Forcke", just: {
-          number: 178,
+        images: ["/src/images/memory/04/IMG-20250807-WA0093.jpg", "/src/images/memory/04/IMG-20250807-WA0091.jpg", "/src/images/memory/04/IMG-20250807-WA0092.jpg"], location: "Centennial, CO", title: "In Memory of Naomi Alene: Funeral Support", target: "65K", raised: "56,380", donations: "402",
+        percentage: "87%", description: `Very long description`, organizer: "Dawit Agonafer", just: {
+          number: 90,
           top: [
-            { name: "Elizabeth Ann Kuhns-Boyle", amount: "200", rank: "First donation", time: "16 mins" },
+            { name: "Dawit Agonafer", amount: "1000", rank: "First donation", time: "16 mins" },
 
-            { name: "Emily Branch", amount: "100", time: "30 mins" },
+            { name: "Anonymous", amount: "50", time: "2 hrs" },
 
-            { name: "Thomas Layman", amount: "25", time: "1 hr" },
+            { name: "Anonymous", amount: "50", time: "3 hrs" },
           ],
           upper: [
             { name: "Jennifer Hall", amount: "50", rank: "Recent donation", time: "16 mins" },
@@ -175,6 +196,29 @@ From the bottom of our hearts, thank you.
           ]
         }
       },
+      {
+        images: ["/src/images/memory/03/IMG-20250807-WA0089.jpg", "/src/images/memory/03/IMG-20250807-WA0090.jpg", "/src/images/memory/03/IMG-20250807-WA0094.jpg"], location: "Stamford, CT", title: "Help Lay Rest 12 year old Brisley Reyes", target: "45K", raised: "31,698", donations: "628",
+        percentage: "70%", description: `Very long description`, organizer: "Olga Reyes Gramajo and BRISLY REYES", just: {
+          number: 244,
+          top: [
+            { name: "Justin Duarte", amount: "100", rank: "First donation", time: "16 mins" },
+
+            { name: "Anonymous", amount: "25", time: "35 mins" },
+
+            { name: "Strawberry Hill PTO", amount: "200", time: "3 hrs" },
+          ],
+          upper: [
+            { name: "Jennifer Hall", amount: "50", rank: "Recent donation", time: "16 mins" },
+            { name: "Edythe Synder Sanders", amount: "25", rank: "Recent donation", time: "36 mins" },
+            { name: "Christopher Parsons", amount: "10", rank: "Recent donation", time: "40 mins" },
+            { name: "Christopher Youngman", amount: "25", rank: "Recent donation", time: "50 mins" },
+          ],
+          others: [
+
+          ]
+        }
+      },
+     
     ]
   },
 ];
@@ -268,13 +312,13 @@ function handleStories() {
                 <div class="stories">
 ${ preview.map(el => {
         const story = `<div class="story">
-                        <div class="story-image"><img src="${el.images || "/src/images/logo.jpg"}" alt=""></div>
+                        <div class="story-image"><img src="${el.images[0] || "/src/images/logo.jpg"}" alt=""></div>
 
                         <div class="story-info">
                             <p class="story-location">${el.location}</p>
                             <p class="story-title">${el.title}</p>
                             <div class="wrapper">
-                                <div class="meter"></div>
+                                <div class="meter" style="width:${el.percentage}"></div>
                             </div>
 
                             <p class="story-amount-raised">$${el.raised} raised</p>
