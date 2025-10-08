161f7e19d7af0a249a2ffb3ab2a299f62cee98f2
Qal Tech <130528093+qal-technologies@users.noreply.github.com>
2025-10-08 10:46:49 +0100

    poiuyre

--- js/general.js
+++ js/general.js
@@ -542,11 +542,11 @@ async function initializeApp() {
     runTicker();
     let loadUser;
 
-    if ("serviceWorker" in navigator) {
-        navigator.serviceWorker.register("/service-worker.js").catch(error => {
-            console.error("Service Worker registration failed:", error);
-        });
-    }
+    // if ("serviceWorker" in navigator) {
+    //     navigator.serviceWorker.register("/service-worker.js").catch(error => {
+    //         console.error("Service Worker registration failed:", error);
+    //     });
+    // }
 
     handleInputFocusFix();
 
