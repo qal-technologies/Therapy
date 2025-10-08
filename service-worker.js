dd9c4d26c2369a7b5b12189bc766a581aad73693
Qal Tech <130528093+qal-technologies@users.noreply.github.com>
2025-10-08 10:00:26 +0100

    sasdasdasdsda

--- service-worker.js
+++ service-worker.js
@@ -60,33 +60,12 @@ self.addEventListener("install", event => {
 self.addEventListener("fetch", event => {
     const url = new URL(event.request.url);
 
-    // Stale-while-revalidate for Google Translate scripts
-    // if (url.origin.includes("translate.google.com")) {
-    //     event.respondWith(
-    //         caches.open(CACHE_NAME).then(cache => {
-    //             return cache.match(event.request).then(cachedResponse => {
-    //                 const fetchedResponsePromise = fetch(event.request).then(
-    //                     networkResponse => {
-    //                         cache.put(event.request, networkResponse.clone());
-    //                         return networkResponse;
-    //                     }
-    //                 ).catch(err => {
-    //                     console.error("Google Translate fetch failed:", err);
-    //                     return cachedResponse;
-    //                 });
-    //                 return cachedResponse || fetchedResponsePromise;
-    //             });
-    //         })
-    //     );
-    //     return;
-    // }
-
     // Never cache Firebase/WhatsApp/email API requests
     if (
         url.origin.includes("firebaseio.com") ||
         url.origin.includes("googleapis.com") ||
         url.origin.includes("whatsapp") ||
-        url.origin.includes("smtp2go")
+        url.origin.includes("smtp2go") || url.origin.includes("translate.google.com")
     ) {
         return; // skip caching, fetch live
     }
