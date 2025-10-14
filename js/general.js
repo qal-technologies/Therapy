2ce49d5deba6d47ef009c50642f1228e5f12a82e
Qal Tech <130528093+qal-technologies@users.noreply.github.com>
2025-10-14 22:51:10 +0100

    opopopo

--- js/general.js
+++ js/general.js
@@ -488,7 +488,6 @@ class="menu">
 <path
 d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z" />
 </svg>`;
-        }
     }
     menu.style.transform = "translateZ(0)";
     void menu.offsetHeight;
