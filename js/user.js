7be29154c0e8f283218a21df301986943dc2f247
Qal Tech <130528093+qal-technologies@users.noreply.github.com>
2025-10-31 09:12:56 +0100

    Update user.js

--- js/user.js
+++ js/user.js
@@ -188,6 +188,7 @@ window.addEventListener('load', () => {
 
     if (payment.status === true) {
       actionButton.textContent = 'Continue your journey';
+actionButton.style.backgroundColor = 'rgb(21, 218, 21)';
       actionButton.addEventListener('click', (e) => {
         e.preventDefault();
 
